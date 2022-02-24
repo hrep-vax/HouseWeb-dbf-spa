582ed75e3362d7b1173909a19b7c73176b6d2768
lei-pot <81949481+lei-pot@users.noreply.github.com>
2022-02-24 20:57:03 +0800

    mas malinis na view page
    mas malinis na view page

--- src/pages/Layout/DashboardNavbar.vue
+++ src/pages/Layout/DashboardNavbar.vue
@@ -6,34 +6,20 @@
     :transparent="true"
   >
     <div slot="brand" class="navbar-wrapper">
-      <div class="navbar-minimize d-inline"><sidebar-toggle-button /></div>
-      <div
-        class="navbar-toggle d-inline"
-        :class="{ toggled: $sidebar.showSidebar }"
-      >
-        <button type="button" class="navbar-toggler" @click="toggleSidebar">
-          <span class="navbar-toggler-bar bar1"></span>
-          <span class="navbar-toggler-bar bar2"></span>
-          <span class="navbar-toggler-bar bar3"></span>
-        </button>
-      </div>
+      
+      
+       
+     
       <a class="navbar-brand" href="#pablo">{{ routeName }}</a>
     </div>
 
     <ul class="navbar-nav" :class="$rtl.isRTL ? 'mr-auto' : 'ml-auto'">
-      <div class="search-bar input-group" @click="searchModalVisible = true">
+      <div class="search-bar input-group">
         <!--
           <input type="text" class="form-control" placeholder="Search...">
           <div class="input-group-addon"><i class="tim-icons icon-zoom-split"></i></div>
         -->
-        <button
-          class="btn btn-link"
-          id="search-button"
-          data-toggle="modal"
-          data-target="#searchModal"
-        >
-          <i class="tim-icons icon-zoom-split"></i>
-        </button>
+       
         <!-- You can choose types of search input -->
       </div>
       <modal
@@ -59,32 +45,8 @@
         title-classes="nav-link"
         class="nav-item"
       >
-        <template
-          slot="title"
-        >
-          <div class="notification d-none d-lg-block d-xl-block"></div>
-          <i class="tim-icons icon-sound-wave"></i>
-          <p class="d-lg-none">New Notifications</p>
-        </template>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item"
-            >Mike John responded to your email</a
-          >
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">You have 5 more tasks</a>
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item"
-            >Your friend Michael is in town</a
-          >
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">Another notification</a>
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">Another one</a>
-        </li>
+        
+       
       </base-dropdown>
       <base-dropdown
         tag="li"
@@ -101,13 +63,6 @@
           <b class="caret d-none d-lg-block d-xl-block"></b>
           <p class="d-lg-none">Log out</p>
         </template>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">Profile</a>
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">Settings</a>
-        </li>
-        <div class="dropdown-divider"></div>
         <li class="nav-link">
           <a href="#" class="nav-item dropdown-item"  v-on:click="logout">Log out</a>
         </li>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,13 +3,6 @@ import AuthLayout from 'src/pages/Pages/AuthLayout.vue';
 // GeneralViews
 import NotFound from 'src/pages/GeneralViews/NotFoundPage.vue';
 
-// Calendar
-const Calendar = () =>
-  import(/* webpackChunkName: "extra" */ 'src/pages/Calendar/CalendarRoute.vue');
-// Charts
-const Charts = () =>
-  import(/* webpackChunkName: "dashboard" */ 'src/pages/Charts.vue');
-
 
 
 // Dashboard pages
@@ -17,19 +10,9 @@ const Dashboard = () =>
   import(/* webpackChunkName: "dashboard" */ 'src/pages/Dashboard/Dashboard.vue');
 import Widgets from 'src/pages/Widgets.vue';
 
-// Forms pages
-const RegularForms = () => import('src/pages/Forms/RegularForms.vue');
-const ExtendedForms = () => import('src/pages/Forms/ExtendedForms.vue');
-const ValidationForms = () => import('src/pages/Forms/ValidationForms.vue');
-const Wizard = () => import('src/pages/Forms/Wizard.vue');
 
-// Maps pages
-const GoogleMaps = () =>
-  import(/* webpackChunkName: "extra" */ 'src/pages/Maps/GoogleMaps.vue');
-const FullScreenMap = () =>
-  import(/* webpackChunkName: "extra" */ 'src/pages/Maps/FullScreenMap.vue');
-const VectorMaps = () =>
-  import(/* webpackChunkName: "extra" */ 'src/pages/Maps/VectorMaps.vue');
+
+
 
 //Pages
 // const User = () =>
@@ -37,8 +20,7 @@ const VectorMaps = () =>
 const User = () =>
   import(/* webpackChunkName: "pages" */ 'src/pages/Pages/CheckViewForm.vue');
 
-const Pricing = () =>
-  import(/* webpackChunkName: "pages" */ 'src/pages/Pages/Pricing.vue');
+
 const TimeLine = () =>
   import(/* webpackChunkName: "pages" */ 'src/pages/Pages/TimeLinePage.vue');
 const Login = () =>
@@ -47,17 +29,6 @@ const Register = () =>
   import(/* webpackChunkName: "pages" */ 'src/pages/Pages/Register.vue');
 const RTL = () =>
   import(/* webpackChunkName: "pages" */ 'src/pages/Pages/RTLPage.vue');
-const Lock = () =>
-  import(/* webpackChunkName: "pages" */ 'src/pages/Pages/Lock.vue');
-
-// TableList pages
-const RegularTables = () =>
-  import(/* webpackChunkName: "tables" */ 'src/pages/Tables/RegularTables.vue');
-const ExtendedTables = () =>
-  import(/* webpackChunkName: "tables" */ 'src/pages/Tables/ExtendedTables.vue');
-const PaginatedTables = () =>
-  import(/* webpackChunkName: "tables" */ 'src/pages/Tables/PaginatedTables.vue');
-
 
 let pagesMenu = {
   path: '/pages',
@@ -121,16 +92,8 @@ const routes = [
         name: 'Dashboard',
         components: { default: Dashboard }
       },
-      {
-        path: 'calendar',
-        name: 'Calendar',
-        components: { default: Calendar }
-      },
-      {
-        path: 'charts',
-        name: 'Charts',
-        components: { default: Charts }
-      },
+   
+     
       {
         path: 'widgets',
         name: 'Widgets',
