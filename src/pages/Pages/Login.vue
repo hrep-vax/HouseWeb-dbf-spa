2f45894733facabd6a7e677e7f2e4a057bd1b906
lei-pot <81949481+lei-pot@users.noreply.github.com>
2022-03-22 19:32:18 +0800

    redirect to user page and upload page
    redirect to user page and upload page

--- src/pages/Pages/Login.vue
+++ src/pages/Pages/Login.vue
@@ -1,76 +1,64 @@
 <template>
-  <div class="container">
-    <div class="col-lg-4 col-md-6 ml-auto mr-auto">
-      <ValidationObserver ref="loginForm">
-        <form >
-          <card class="card-login card-white">
-            <template slot="header">
-              <img src="img/card-primary.png" alt="" />
-              <h1 class="card-title">Log in</h1>
-            </template>
+<div class="container">
+  <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+    <ValidationObserver ref="loginForm">
+      <form>
+        <card class="card-login card-white">
+          <template slot="header">
+            <img src="img/card-primary.png" alt="" />
+            <h1 class="card-title">Log in</h1>
+          </template>
 
-            <div>
-              <ValidationProvider
-                name="hrep_id"
-                rules="required"
-                v-slot="{ passed, failed, errors }"
-              >
-              <base-input
-                required
-                v-model="hrep_id"
-                type="hrep_id"
-                placeholder="HREP ID"
-                addon-left-icon="tim-icons icon-badge"
-                :error="errors[0]"
-                :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
+          <div>
+            <ValidationProvider name="hrep_id" rules="required" v-slot="{ passed, failed, errors }">
+              <base-input required v-model="hrep_id" type="hrep_id" placeholder="HREP ID" addon-left-icon="tim-icons icon-badge" :error="errors[0]" :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
               </base-input>
-             </ValidationProvider>
-
-             <ValidationProvider
-               name="password"
-               rules="required|min:5"
-               v-slot="{ passed, failed, errors }"
-             >
-             <base-input
-               required
-               v-model="password"
-               placeholder="Password"
-               addon-left-icon="tim-icons icon-lock-circle"
-               type="password"
-               :error="errors[0]"
-               :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
-             </base-input>
             </ValidationProvider>
+
+            <ValidationProvider name="password" rules="required|min:5" v-slot="{ passed, failed, errors }">
+              <base-input required v-model="password" placeholder="Password" addon-left-icon="tim-icons icon-lock-circle" type="password" :error="errors[0]" :class="[{ 'has-success': passed }, { 'has-danger': failed }]">
+              </base-input>
+            </ValidationProvider>
+          </div>
+
+          <div slot="footer">
+            <base-button native-type="submit" type="primary" class="mb-3" size="lg" @click="submit" block>
+              Login
+            </base-button>
+            <div class="pull-left">
+              <h6>
+                <router-link class="link footer-link" to="/register">
+                  Create Account
+                </router-link>
+              </h6>
             </div>
 
-            <div slot="footer">
-              <base-button native-type="submit" type="primary" class="mb-3" size="lg" @click="submit" block>
-                Login
-              </base-button>
-              <div class="pull-left">
-                <h6>
-                  <router-link class="link footer-link" to="/register">
-                    Create Account
-                  </router-link>
-                </h6>
-              </div>
+            <div class="pull-right">
 
-              <div class="pull-right">
-               
-              </div>
             </div>
-          </card>
-        </form>
-      </ValidationObserver>
-    </div>
+          </div>
+        </card>
+      </form>
+    </ValidationObserver>
   </div>
+</div>
 </template>
+
 <script>
 import Vue from 'vue'
-import { extend } from "vee-validate";
-import { required, min } from "vee-validate/dist/rules";
-import {mapActions} from 'vuex'
-import { handleVuexApiCall } from 'src/util/helper'
+import {
+  extend
+} from "vee-validate";
+import {
+  required,
+  min
+} from "vee-validate/dist/rules";
+import {
+  mapActions
+} from 'vuex'
+import {
+  handleVuexApiCall
+} from 'src/util/helper'
 extend("min", min);
 
 extend("required", required);
@@ -85,20 +73,44 @@ export default {
   },
   methods: {
     ...mapActions(['handleLogin']),
-   async submit() {
-     
-          const valid = await this.$refs.loginForm.validate()
+    async submit() {
+
+      const valid = await this.$refs.loginForm.validate()
       if (!valid) return
 
       this.isLoading = true
-      const result = await handleVuexApiCall(this.handleLogin, {hrep_id: this.hrep_id, password: this.password})
+      const result = await handleVuexApiCall(this.handleLogin, {
+        hrep_id: this.hrep_id,
+        password: this.password
+      })
 
-      if (result.success) this.$router.push({name: 'Pages'}).catch(err => err)
-      else Vue.$toast.open({ message: result.error.message, type: result.error.type })
+      // get role redirect to User Page if regular user else direct to Upload Page if uploader
+      const user = JSON.parse(localStorage.getItem('WEB_APP_KIT_USER'))
+
+      if (user.user_roles == "regular") {
+        if (result.success) this.$router.push({
+          name: 'Pages'
+        }).catch(err => err)
+        else Vue.$toast.open({
+          message: result.error.message,
+          type: result.error.type
+        })
+
+      } else {
+        if (result.success) this.$router.push({
+          name: 'Upload Page'
+        }).catch(err => err)
+        else Vue.$toast.open({
+          message: result.error.message,
+          type: result.error.type
+        })
+      }
+
     }
   }
 };
 </script>
+
 <style>
 .navbar-nav .nav-item p {
   line-height: inherit;
