3371db3429bde1464116131c5de29c3d8e3895a0
lei-pot <81949481+lei-pot@users.noreply.github.com>
2022-03-31 14:35:26 +0800

    change icon
    change icon

--- src/pages/Layout/DashboardNavbar.vue
+++ src/pages/Layout/DashboardNavbar.vue
@@ -59,7 +59,7 @@
         <template
           slot="title"
         >
-          <div> <i class="tim-icons icon-lock-circle"></i></div>
+          <div> <i class="tim-icons icon-single-02"></i></div>
           <b class="caret d-none d-lg-block d-xl-block"></b>
           <p class="d-lg-none">Log out</p>
         </template>
